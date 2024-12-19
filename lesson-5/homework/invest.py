35c139ddf4f58c6d2b47ee5ef1109c019fc6ad3d
agtursunov <agtursunov@gmail.com>
2024-12-19 19:31:07 +0500

    some code is changed

--- lesson-5/homework/invest.py
+++ lesson-5/homework/invest.py
@@ -1,5 +1,6 @@
 def invest(amount, rate, years):
     for i in range(1, int(years + 1)):
-        print(f'year i: ${round(amount * (1 + rate)**i, 2)}')
+        print(f'year {i}: ${round(amount * (1 + rate)**i, 2)}')
+
 amount_user, rate_user, years_user = map(float, input('Enter an initial amount, an annual percentage rate, and a number of years with space: ').split())
 invest(amount_user, rate_user, years_user)
